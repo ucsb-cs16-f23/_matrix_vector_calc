932e1daff81f82b3b4d71c3eb2db47a82107ccf2
Hanyu Hu <hanyuhuss@163.com>
2023-11-18 00:30:53 -0800

    _

---
+++ command_a.cpp
@@ -0,0 +1,813 @@
+﻿#include "command_a.h"
+#include<cstring>
+#include<iostream>
+#include"my_Vars.h"
+#include<vector>
+
+char cmNode::op_table[256];
+using namespace std;
+list<MyVar>mVar_list;
+vector<FuncBase*> function_list;
+char* _My_Stack_Poiner;
+char* _m_stack_end;
+char* _m_stack_begin;
+void* FuncBase::ret_area;
+char cmNode::ret_char_buffer[128];
+char _m_shared_left[3] = "(";
+char _m_shared_right[3] = ")";
+
+
+int cmNode::mov_to_next(char*& _str)
+{
+	int cnt = 0;
+	char* str = _str;
+	bool flag_break = false;
+	while (*str)
+	{
+		switch (op_table[*str])
+		{
+		case 5:
+			++str;
+			flag_break = true;
+			break;
+		case chBound:
+			if (cnt == 0){
+				++cnt;
+				++str;
+			}
+			flag_break = true;
+			break;
+		case chOp_1:
+		{
+			if (cnt)flag_break=true;
+			else {
+				cnt = op_length(str);
+				str += cnt;
+				flag_break = true;
+			}
+		}
+			break;
+		default:
+			break;
+		}
+		if (flag_break)break;
+		++str;
+		++cnt;
+	}
+	_str = str;
+	return cnt;
+}
+
+void cmNode::clean_up()
+{
+	cmNode* ptr=this->pNext;
+	cmNode* pFree;
+	while (ptr)
+	{
+		pFree = ptr;
+		ptr = ptr->pNext;
+		delete pFree;
+	}
+}
+
+cmNode::cmNode()
+{
+	pPrev = pNext = nullptr;
+	type = idUNDEFINED;
+	str = nullptr;
+}
+
+cmNode::cmNode(char* _str,int len)
+{
+	str = _str;
+	pPrev = NULL;
+	pNext = nullptr;
+	switch (op_table[_str[0]])
+	{
+	case 0:
+		type = mIDENTIFIER;
+		break;
+	case chOp_1:
+		type = mOPERATOR;
+		break;
+	case chBound:
+		type = mType::mExprEND;
+		break;
+	default:
+		type = idUNDEFINED;
+		break;
+	}
+	if (type == mIDENTIFIER) {
+		if (str[0] >= '0' && str[0] <= '9'){
+			type = mVALUE; 
+			goto func_end;
+		}
+		get_cstr();
+		for (auto& a : mVar_list) {
+			if (strcmp(a.var_name, ret_char_buffer) == 0) {
+				type = idVAR;
+				goto func_end;
+			}
+		}
+		for (auto a : function_list) {
+			if (strcmp(a->name, ret_char_buffer) == 0) {
+				type = idFUNC;
+				goto func_end;
+			}
+		}
+		type = idUNDEFINED;
+	}
+	if (type == mOPERATOR) {
+		switch (str[0])
+		{
+		case '=':
+			if (length == 2)type = mOPCMP;
+			break;
+		case '<':
+		case '>':
+			type = mOPCMP;
+			break;
+		case '!':
+			type = mOPLOG;
+			break;
+		case '|':
+		case '&':
+			if (length == 2)type = mOPLOG;
+			break;
+		default:
+			break;
+		}
+	}
+	func_end:
+	length = len;
+}
+
+cmNode::~cmNode()
+{
+	clean_up();
+}
+
+void cmNode::append(cmNode* nd)
+{
+	auto ptr = find_back();
+	ptr->pNext = nd;
+	nd->pPrev = ptr;
+}
+
+cmNode* cmNode::find_back()
+{
+	auto ptr = this;
+	while ((ptr->pNext))
+	{
+		ptr = ptr->pNext;
+	}
+	return ptr;
+}
+
+cmNode* cmNode::find_head()
+{
+	auto ptr = this;
+	while ((ptr->pPrev))
+	{
+		ptr = ptr->pPrev;
+	}
+	return ptr;
+}
+
+int cmNode::op_length(char* str)
+{
+	if (str[0] == '-') {
+		if (str[1] && str[1] == '>')
+			return 2;
+	}
+	if (str[0] == '=') {
+		if (str[1] && str[1] == '=')
+			return 2;
+	}
+	if (str[0] == '|') {
+		if (str[1] && str[1] == '|')
+			return 2;
+	}
+	if (str[0] == '<') {
+		if (str[1] && str[1] == '=')
+			return 2;
+	}
+	if (str[0] == '>') {
+		if (str[1] && str[1] == '=')
+			return 2;
+	}
+	if (str[0] == '&') {
+		if (str[1] && str[1] == '&')
+			return 2;
+	}
+	return 1;
+}
+
+cmNode* cmNode::create_list(char* _str)
+{
+	cmNode Head;
+	cmNode* ptr = &Head;
+	char* p_ass = _str;
+	int cnt;
+	while(*_str) {
+		while (*_str==' ')++_str;//ignore space
+		p_ass = _str;
+		cnt = mov_to_next(_str);
+		if (cnt == 0)continue;
+		ptr->append(new cmNode(p_ass, cnt));
+		ptr = ptr->pNext;
+	}
+
+	ptr = Head.pNext;
+	ptr->_break_list();
+	return ptr;
+}
+
+void cmNode::print_node()
+{
+	char buffer[32];
+	cmNode* PTR = this;
+	while (PTR)
+	{
+		memcpy_s(buffer, 32, PTR->str, PTR->length);
+		buffer[PTR->length] = '\0';
+		std::cout << buffer<<' ';
+		PTR = PTR->pNext;
+	}
+	std::cout << std::endl;
+}
+
+void cmNode::_break_list()
+{
+	if (pPrev)pPrev->pNext = nullptr;
+	pPrev = nullptr;
+}
+
+void cmNode::table_init()
+{
+	memset(op_table, 0, 256);
+	op_set('=', chOp_1);
+	op_set('+', chOp_1);
+	op_set('-', chOp_1);
+	op_set('*', chOp_1);
+	op_set('/', chOp_1);
+	op_set('(', chOp_1);
+	op_set(')', chBound);
+	op_set('>', chOp_1);
+	op_set('<', chOp_1);
+	op_set('|', chOp_1);
+	op_set('&', chOp_1);
+	op_set('?', chOp_1);
+	op_set(']', chOp_1);
+	op_set('[', chOp_1);
+	op_set(':', chBound);
+
+	op_set(',', chBound);
+	op_set(' ', 5);
+}
+
+const char* cmNode::get_cstr() const
+{
+	memcpy_s(ret_char_buffer, 128, str, length);
+	ret_char_buffer[length] = '\0';
+	return ret_char_buffer;
+}
+
+void cmNode::insert(cmNode* _Node)
+{
+	if (pPrev) {
+		pPrev->pNext = _Node;
+	}
+	_Node->pPrev = pPrev;
+	pPrev = _Node;
+	_Node->pNext = this;
+}
+
+FuncBase::FuncBase()
+{
+	name = new  char[30];
+	strcpy_s(name, 30, "nana");
+	cnt = 0;
+	m_type_list = NULL;
+}
+
+FuncBase::FuncBase(const char* _name)
+{
+	auto len = strlen(_name) + 1;
+	name = new  char[len];
+	strcpy_s(name, len,_name);
+	cnt = 0;
+	m_type_list = NULL;
+	is_ptr_list = NULL;
+}
+
+int FuncBase::get_arguments_cnts()
+{
+	return  cnt;
+}
+
+MyVar::_m_type FuncBase::get_arg_type(int a_at)
+{
+	return m_type_list[a_at];
+}
+
+void FunctionClassINIT(unsigned dwStackSize, unsigned dwRetAsize)
+{
+	_m_stack_end = (char*)operator new(dwStackSize);
+	_m_stack_begin = _m_stack_end + dwStackSize;
+	_My_Stack_Poiner = _m_stack_begin;
+	FuncBase::ret_area = operator new(dwRetAsize);
+}
+
+void determine_sequence(cmNode* _expr) {
+	cmNode* o_expr = _expr;
+	while (_expr)
+	{
+		if (_expr->str[0] == '*' || _expr->str[0] == '/') {
+			_expr->get_prev()->insert(new cmNode(_m_shared_left, 1));
+			int cnt_left = 0;
+			while (!(_expr->is_end()))
+			{
+				if (_expr->str[0] == '(') ++cnt_left;
+				if (_expr->str[0] == ')') {
+					if (cnt_left == 0)break;
+					--cnt_left;
+				}
+				if (_expr->str[0] == '+'&&cnt_left == 0) break;
+				if (_expr->str[0] == '-') {
+					if (cnt_left == 0 &&
+						_expr->get_prev()->str[0] != '*' &&
+						_expr->get_prev()->str[0] != '/')break;
+				}
+				_expr = _expr->get_next();
+			}
+			if ((_expr->is_end()))_expr->append(new cmNode(_m_shared_right, 1));
+			else _expr->insert(new cmNode(_m_shared_right, 1));
+
+		}
+		_expr = _expr->get_next();
+	}
+	_expr = o_expr;
+	while (!(_expr->is_end()))
+	{
+		if (_expr->type == cmNode::mOPLOG && _expr->str[0] != '!') {
+			cmNode* _x = _expr;
+			_expr->insert(new cmNode(_m_shared_right, 1));
+			_x = _x->get_prev()->get_prev();
+			int rig_cnt = 0;
+			while (!(_x->is_head()))
+			{
+				if (_x->str[0] == ')')++rig_cnt;
+				if (_x->str[0] == '(') {
+					if (rig_cnt)--rig_cnt;
+					else break;
+				}
+				if (_x->type == cmNode::mOPLOG && _expr->str[0] != '!'
+					&& rig_cnt == 0)break;
+				_x = _x->get_prev();
+			}
+			if ((_x->is_head()))_x->insert(new cmNode(_m_shared_left, 1));
+		}
+		_expr = _expr->get_next();
+	}
+	while (!(_expr->is_head()))
+	{
+		if (_expr->type == cmNode::mOPLOG && _expr->str[0] != '!') {
+			_expr = _expr->get_next();
+			_expr->insert(new cmNode(_m_shared_left, 1));
+			_expr->find_back()->append(new cmNode(_m_shared_right, 1));
+		}
+		_expr = _expr->get_prev();
+	}
+}
+
+MyVar CreateVector(char* _A1)
+{
+	MyVar ret;
+	int _cnt  = 0;
+	char* ap;
+	_VA_start(ap, _A1);
+	while (is_VA_end(ap))
+	{
+		ap -= sizeof(MyVar);
+		++_cnt;
+	}
+	_VA_start(ap,_A1);
+	ret.assign_val(Vector(_cnt));
+	for (auto& a : *ret.my_data.vec) {
+		if (_VA_arg<MyVar>(ap).var_type != MyVar::number)
+			throw "^CREATE_VECTOR^, unable to assign value";
+		a = *_VA_arg<MyVar>(ap).my_data.num;
+	}
+	return ret;
+}
+
+void procCommand_1(char* _command)
+{
+	auto len = strlen(_command);
+	cmNode* _expr = NULL;
+	MyVar vr;
+	if (len > 10 && strncmp(_command, "create_var", 10) == 0) {
+		_command += 11;
+		_expr = cmNode::create_list(_command);
+		CalcExpr_L(_expr);
+		len = 0;
+		while (_command[len] && _command[len] != ' ')
+			++len;
+		string str(_command, len);
+		mVar_list.push_back(MyVar(MyVar::null_var, str.c_str(), NULL));
+		_expr = cmNode::create_list(_command);
+		vr = mVar_list.back()= CalcExpr_L(_expr);
+		goto func_end;
+	}
+	_expr = cmNode::create_list(_command);
+	vr = CalcExpr_L(_expr);
+func_end:
+	switch (vr.var_type)
+	{
+	case MyVar::matrix:
+		vr.my_data.mat->print_matrix();
+		break;
+	case MyVar::vector:
+		for (auto& a : *vr.my_data.vec)cout << a << ' ';
+		cout << endl;
+		break;
+	case MyVar::number:
+		cout << vr.my_data.num[0] << endl;
+		break;
+	default:
+		cout << endl;
+	}
+	tmp_var_list.clear();
+}
+
+void implFuncInit()
+{
+	function_list.push_back(new MYVA_FUNC(CreateVector, "CreateVector"));
+	function_list.push_back(new MYFUNC(REF, "REF"));
+}
+
+//IMPORTANT function stack not decleared !
+MyVar* FindVar(const char* name) {
+	for (auto& a : mVar_list) {
+		if (strcmp(a.var_name, name) == 0) {
+			return&a;
+		}
+	}
+	return 0;
+}
+
+//HELPER FUNCTION   (●'◡'●)
+void _M_helper_calcexpr(MyVar* ret, bool& flag_neg, char& _m_op, MyVar& ptr_vr) {
+	if (flag_neg)ptr_vr.set_negetive();
+	switch (_m_op)
+	{
+	case '+':
+		ret->op_add(ptr_vr);
+		break;
+	case '*':
+		ret->op_mul(ptr_vr);
+		break;
+	case '/':
+		ret->my_data.num->operator/=(*ptr_vr.my_data.num);
+		break;
+	default:
+		break;
+	}
+	_m_op = '+';
+	flag_neg = false;
+};
+
+//^^^ READY!^^^  <(￣︶￣)↗[GO!]
+void _MS_pushVAL(MyVar& _val,bool is_ptr) {
+	switch (_val.var_type)
+	{
+	case MyVar::number:
+		if (is_ptr) PUSH<MyNum*>(_val.my_data.num);
+		else PUSH<MyNum>(*_val.my_data.num);
+		break;
+	case MyVar::vector:
+		if (is_ptr) PUSH<Vector*>(_val.my_data.vec);
+		else PUSH<Vector>(*_val.my_data.vec);
+		break;
+	case MyVar::matrix:
+		if (is_ptr) PUSH<Matrix*>(_val.my_data.mat);
+		else PUSH<Matrix>(*_val.my_data.mat);
+		break;
+	default:
+		break;
+	}
+}//^^^___^^^ ARG_IN_STACK!
+
+//Push rvalue
+void _MS_pushVAL_R(MyVar&& _val) {
+	switch (_val.var_type)
+	{
+	case MyVar::number:
+		PUSH<MyNum>(std::move(*_val.my_data.num));
+		break;
+	case MyVar::vector:
+		PUSH<Vector>(std::move(*_val.my_data.vec));
+		break;
+	case MyVar::matrix:
+		 PUSH<Matrix>(std::move(*_val.my_data.mat));
+		break;
+	default:
+		break;
+	}
+}
+
+void ExxCallFunc(cmNode*& ptr_expr, FuncBase* fptr) {
+	//passing arguments
+	cmNode* _expr = ptr_expr;
+	if ((fptr->_is_va_func()))goto _call_va_func;
+	for (int i = 0; i < fptr->get_arguments_cnts(); ++i) {
+		if ((fptr->arg_is_pointer_at(i))) {
+			//arg is reference, lvalue required
+			MyVar* x = FindVar(_expr->get_cstr());
+			if (x == nullptr || _expr->get_next()->type != cmNode::mExprEND) {
+				throw "func require lvalue";
+			}
+			if (fptr->get_arg_type(i) != x->var_type)throw "Function BAD call with error arg type";
+			_MS_pushVAL(*x, true);
+			_expr = _expr->get_next();
+			if (!(_expr->is_end()))_expr = _expr->get_next();
+		}
+		else {
+			MyVar&& x = CalcExpr_L(_expr);
+			if(i!=fptr->get_arguments_cnts()-1)_expr = _expr->get_next();
+			if (fptr->get_arg_type(i) != x.var_type)throw "Function BAD call with error arg type";
+			_MS_pushVAL_R(std::move(x));
+		}
+	}
+	goto _func_ret;
+_call_va_func:
+	fptr->record_stack_pointer();
+	while (_expr->str[0]!=')')
+	{
+		_MS_pushVAL_R(CalcExpr_L(_expr));
+	}
+	_func_ret:
+	ptr_expr = _expr;
+	//call Function;
+	fptr->_call_func_ret_on_retarea();
+}
+
+//Function Caller for rvalue return function (*/ω＼*)
+//* if the function returns lvalue, it treated as rvalue function
+MyVar RvalRetFunctionCaller(cmNode*& ptr_expr) {
+	cmNode* _expr = ptr_expr;
+	FuncBase* fptr = NULL;
+	//find function//
+	{
+		auto cname = _expr->get_cstr();
+		for (auto a : function_list) {
+			if (strcmp(cname, a->name)) {
+				fptr = a;
+				break;
+			}
+		}
+	}
+	_expr = _expr->get_next()->get_next();
+	ExxCallFunc(_expr, fptr);
+	ptr_expr = _expr;
+	MyVar ret;
+	if (fptr->f_return_pointer) ret = **reinterpret_cast<MyVar**>(FuncBase::ret_area);
+	else ret = ::std::move(*reinterpret_cast<MyVar*>(FuncBase::ret_area));
+	return ret;
+}
+//wwwwwwwww~
+
+MyVar* LvalFuncCaller(cmNode*& ptr_expr) {
+	cmNode* _expr = ptr_expr;
+	FuncBase* fptr = NULL;
+	{
+		auto cname = _expr->get_cstr();
+		for (auto a : function_list) {
+			if (strcmp(cname, a->name)) {
+				fptr = a;
+				break;
+			}
+		}
+	}
+	_expr = _expr->get_next()->get_next();
+	ExxCallFunc(_expr, fptr);
+	ptr_expr = _expr;
+	return *reinterpret_cast<MyVar**>(FuncBase::ret_area);
+}
+
+MyVar CalcExpr_L(cmNode*& ptr_expr) {
+	MyVar *m_lvalue = NULL;
+	cmNode* _expr = ptr_expr;
+
+	//^^^ find '=' ^^^
+	//GET lvalue, if no lvalue (no '=' ) lvlaue == null
+	while ((_expr) && _expr->type != cmNode::mExprEND) {
+
+		if (_expr->str[0] == '(')break;
+		//treated as rval ,for case such as "a = (b = c) + 1;"
+
+		if (_expr->str[0] == '='&&_expr->length==1) {
+			auto cnme = ptr_expr->get_cstr();
+			if (ptr_expr->type == cmNode::idFUNC) {
+				m_lvalue = LvalFuncCaller(ptr_expr);
+			}
+			else 
+				m_lvalue = FindVar(cnme);
+			_expr = _expr->get_next();
+			break;
+		}
+		_expr = _expr->get_next();
+	}
+
+	MyVar ret;
+	//calculate rval (●'◡'●)
+	if (m_lvalue) {
+		ret =*m_lvalue = std::move(CalcExpr_L(_expr));
+	}
+	else {
+		_expr = ptr_expr;
+		
+		//restore pointer  (≧∇≦)ﾉ☆
+		_expr = ptr_expr;
+		ret = std::move(CalcExpr_R(_expr));
+	}
+	ptr_expr = _expr;
+	return ret;
+}
+
+void _expr_end(cmNode*& ptr_expr) {
+	cmNode* _expr = ptr_expr;
+	int cnt_left = 0;
+	while (_expr&&(cnt_left > 0 || _expr->type != cmNode::mExprEND))
+	{
+		if (_expr->str[0] == '(')++cnt_left;
+		if (_expr->str[0] == ')')--cnt_left;
+		_expr = _expr->get_next();
+	}
+	ptr_expr = _expr;
+}
+
+//assume function is rvalue 
+MyVar CalcExpr_R(cmNode*& ptr_expr)
+{
+	MyVar ret;
+
+	cmNode* _expr = ptr_expr;
+	MyVar tmp_val_r;
+	bool flag_neg = false;
+	char _m_op = '+';
+	bool flag_not = false;
+
+	//go through expression from left to right  (o゜▽゜)o☆
+	while ((_expr) && _expr->type!=cmNode::mExprEND)
+	{
+		switch (_expr->type)
+		{
+		case cmNode::mType::idVAR:
+			tmp_val_r.assign_val<MyVar>(*FindVar(_expr->get_cstr()));
+			if (flag_not)flag_not = false, tmp_val_r._Not();
+			_M_helper_calcexpr(&ret, flag_neg, _m_op, tmp_val_r);
+			break;
+		case cmNode::mType::idFUNC:
+			tmp_val_r = RvalRetFunctionCaller(_expr);
+			if (flag_not)flag_not = false, tmp_val_r._Not();
+			_M_helper_calcexpr(&ret, flag_neg, _m_op, tmp_val_r);
+			break;
+		case cmNode::mType::mVALUE:
+			tmp_val_r.assign_val(MyNum(atoll(_expr->get_cstr())));
+			if (flag_not)flag_not = false, tmp_val_r._Not();//!1 ? is that  meaningful??
+			_M_helper_calcexpr(&ret, flag_neg, _m_op, tmp_val_r);
+			break;
+		case cmNode::mType::mOPERATOR:
+		{
+			switch (_expr->str[0])
+			{
+			case '-':
+				flag_neg = !flag_neg;
+				break;
+			case '(':
+				_expr = _expr->get_next();
+				tmp_val_r = CalcExpr_L(_expr);
+				if (flag_not)flag_not = false, tmp_val_r._Not();
+				_M_helper_calcexpr(&ret, flag_neg, _m_op, tmp_val_r);
+				//recursion to calculate value q(≧▽≦q)
+				break;
+			case '?':
+				_expr = _expr->get_next();
+				//supprt conditional exprission
+				if ((ret.my_data.num->_up)) {
+					//(//true? (...) : (...) )
+					ret = CalcExpr_R(_expr);
+					_expr = _expr->get_next();
+					_expr_end(_expr);
+				}
+				else {
+					while (_expr->str[0]!=':')
+					{
+						_expr = _expr->get_next();
+					}
+					_expr = _expr->get_next();
+					ret = CalcExpr_R(_expr);
+				}
+				goto func_return;
+				break;
+			case '[':
+				//TODO: Create Matrix
+				break;
+			case ']':
+				goto func_return;
+			default:
+				_m_op = _expr->str[0];
+				break;
+			}
+		}
+			break;
+			case cmNode::mOPCMP:
+				switch (_expr->str[0])
+				{
+				case '<':
+					_expr = _expr->get_next();
+					tmp_val_r = CalcExpr_R(_expr);
+					if (ret.var_type != MyVar::number &&
+						tmp_val_r.var_type != MyVar::number)throw "cannot compare...";
+					if (_expr->length == 2)
+						ret.assign_val(MyNum(ret.my_data.num->operator<=(
+							*tmp_val_r.my_data.num)));
+					else
+						ret.assign_val(MyNum(ret.my_data.num->operator<(
+							*tmp_val_r.my_data.num)));
+					goto func_return;
+					break;
+				case '>':
+					_expr = _expr->get_next();
+					tmp_val_r = CalcExpr_R(_expr);
+					if (ret.var_type != MyVar::number &&
+						tmp_val_r.var_type != MyVar::number)throw "cannot compare...";
+					if (_expr->length == 2)
+						ret.assign_val(MyNum(ret.my_data.num->operator>=(
+							*tmp_val_r.my_data.num)));
+					else 
+						ret.assign_val(MyNum(ret.my_data.num->operator>(
+							*tmp_val_r.my_data.num)));
+					goto func_return;
+					break;
+				case '=':
+					tmp_val_r = CalcExpr_R(_expr);
+					ret.assign_val(MyNum(
+						(int)(ret == tmp_val_r)
+					));
+					if (flag_not)flag_not = false, ret._Not();
+					goto func_return;
+					break;
+				}
+				break;
+			case cmNode::mOPLOG:
+				switch (_expr->str[0])
+				{
+				case '|':
+					_expr = _expr->get_next();
+					if (_expr->length == 2) {
+						if (ret.my_data.num->_up != 0) {
+							_expr = _expr->get_next();
+							_expr_end(_expr);
+							break;
+						}
+						tmp_val_r = CalcExpr_R(_expr);
+						ret.assign_val(MyNum(
+							ret.my_data.num->_up || tmp_val_r.my_data.num->_up
+						));
+					}
+					break;
+				case '&':
+					_expr = _expr->get_next();
+					if (_expr->length == 2) {
+						if (ret.my_data.num->_up == 0) {
+							_expr = _expr->get_next();
+							_expr_end(_expr);
+							break;
+						}
+						tmp_val_r = CalcExpr_R(_expr);
+						ret.assign_val(MyNum(
+							ret.my_data.num->_up || tmp_val_r.my_data.num->_up
+						));
+					}
+					break;
+				case '!':
+					flag_not = !flag_not;
+					break;
+				default:
+					break;
+				}
+				break;
+		default:
+			break;
+		}
+		if(_expr)_expr = _expr->get_next();
+	}
+
+	//prepare to return, set ptrヾ(≧▽≦*)o
+	func_return:
+	ptr_expr = _expr;
+	return ret;
+}
---
+++ command_a.h
@@ -0,0 +1,262 @@
+#ifndef _COMMAND_A_H_
+#define _COMMAND_A_H_
+#include<new>
+#include<utility>
+#include"my_Vars.h"
+#include<type_traits>
+
+class cmNode {
+private:
+	cmNode* pPrev;
+	cmNode* pNext;
+	static char op_table[256];
+	static char ret_char_buffer[128];
+	constexpr static  char chBound = 3;
+	constexpr static  char chOp_1 = 1;
+	constexpr static  char chOP_2 = 2;
+	static inline void op_set(char ind, char val) {
+		op_table[ind] = val;
+	}
+	static int mov_to_next(char*& _str);
+	static int op_length(char* str);
+public:
+	char* str;
+	int length;
+	enum mType
+	{
+		mOPERATOR,
+		mIDENTIFIER,
+		mVALUE,
+		idVAR,
+		idFUNC,
+		mExprEND,
+		mOPCMP,
+		mOPLOG,
+		idUNDEFINED
+	};
+	mType type;
+	void clean_up();
+	cmNode();
+	cmNode(char* _str, int);
+	~cmNode();
+	cmNode(const cmNode&) = delete;
+	void append(cmNode* nd);
+	cmNode* find_back();
+	cmNode* find_head();
+
+	static cmNode* create_list(char* _str);
+	void print_node();
+
+	bool is_end() {
+		return pNext == nullptr;
+	}
+	cmNode* get_next() {
+		return pNext;
+	}
+	cmNode* get_prev() {
+		return pPrev;
+	}
+	bool is_head() {
+		return pPrev == nullptr;
+	}
+
+	//prev = 0, prev->next = 0
+	void _break_list();
+	static void table_init();
+	static int word_chk(cmNode* _Node);
+	const char* get_cstr()const;
+	void insert(cmNode* _Node);
+};
+
+#define STACK_PUSHPOP_CHECKLEVEL 1
+
+extern char* _My_Stack_Poiner;
+extern char* _m_stack_end;
+extern char* _m_stack_begin;
+
+template<class T>
+void PUSH(const T& _val) {
+	_My_Stack_Poiner -= sizeof(T);
+#if STACK_PUSHPOP_CHECKLEVEL == 1
+	if (_m_stack_end > _My_Stack_Poiner)throw - 1;
+#endif
+	new(_My_Stack_Poiner) T(_val);
+}
+
+template<class T>
+void PUSH(T&& _val) {
+	_My_Stack_Poiner-= sizeof(T);
+#if STACK_PUSHPOP_CHECKLEVEL == 1
+	if (_m_stack_end > _My_Stack_Poiner)throw - 1;
+#endif
+	new(_My_Stack_Poiner) T(::std::move(_val));
+}
+
+template<class T>
+typename std::remove_const<typename std::remove_reference<T>::type>::type 
+POP() {
+	using Type = typename std::remove_const<typename std::remove_reference<T>::type>::type;
+	Type ret = ::std::move(*reinterpret_cast<Type*>(_My_Stack_Poiner));
+#if STACK_PUSHPOP_CHECKLEVEL == 1
+	if (_My_Stack_Poiner+sizeof(Type) > _m_stack_begin)throw - 1;
+#endif
+	reinterpret_cast<Type*>(_My_Stack_Poiner)->~Type();
+	_My_Stack_Poiner += sizeof(Type);
+	return ret;
+}
+
+template<int _i, class Ft>
+struct _At;
+
+template<class R, class A1, class... Ar>
+struct _At<0, R(A1, Ar...)>
+{
+	using type = A1;
+};
+
+template<int _i, class R, class A1, class... Ar>
+struct _At<_i, R(A1, Ar...)> {
+	using type = typename _At<_i - 1, R(Ar...)>::type;
+};
+
+template<class F>
+struct _Rt;
+
+template<class _Ret, class... Arg>
+struct _Rt<_Ret(Arg...)> {
+	using type = typename _Ret;
+};
+
+class FuncBase {
+public:
+	char* name;
+	static void* ret_area;
+	FuncBase();
+	FuncBase(const char* _name);
+	virtual ~FuncBase() {
+		delete name;
+	}
+	virtual void _call_func_ret_on_retarea()const {
+	}
+	virtual bool _is_va_func()const {
+		return false;
+	}
+	int get_arguments_cnts();
+	MyVar::_m_type get_arg_type(int a_at);
+	bool arg_is_pointer_at(int at) {
+		return is_ptr_list[at];
+	}
+	const char* pr_name() {
+		return name;
+	}
+	bool f_return_pointer;
+	virtual void record_stack_pointer() {
+	}
+protected:
+	int cnt;
+	MyVar::_m_type* m_type_list;
+	bool* is_ptr_list;
+};
+
+template<class FuncTy>class MyFunc;
+
+template<class _ret, class ... Args>
+class MyFunc<_ret(Args...)> :public FuncBase{
+protected:
+	//static_assert(_ret != void, "return type of function is void");
+	_ret(*function_pointer)(Args...);
+	template<int index>
+	constexpr void get_func_arg_list() {
+		m_type_list[index] = MyVar::_get_type_id< 
+			std::remove_const<::std::remove_reference< ::std::remove_pointer<
+			_At<index, _ret(Args...)>
+			::type>::type>::type>::type>();//get argument type~
+		is_ptr_list[index] = ::std::is_pointer<_At<index, _ret(Args...)>>::value;
+		get_func_arg_list<index - 1>();
+
+	}
+	template<>
+	constexpr void get_func_arg_list<-1>(){
+	}
+public:
+	MyFunc(_ret(*fptr)(Args...), const char* _name):FuncBase(_name) {
+		function_pointer = fptr;
+		cnt = sizeof...(Args);
+		m_type_list = new MyVar::_m_type[cnt];
+		is_ptr_list = new bool[cnt];
+		get_func_arg_list<sizeof...(Args) - 1>();
+		f_return_pointer = ::std::is_pointer<_ret>::value;
+	}
+	virtual ~MyFunc(){
+		delete[] m_type_list;
+	}
+	virtual void _call_func_ret_on_retarea()const override {
+		new(ret_area) _ret(function_pointer(POP<Args>()...));
+	}
+	virtual bool _is_va_func()const {
+		return false;
+	}
+};
+
+MyVar CalcExpr_R(cmNode* &_expr);
+
+//assume expression on the left hand side of '=' is lvalue
+//*before use, check ALL lvaue
+MyVar CalcExpr_L(cmNode*& ptr_expr);
+
+void FunctionClassINIT(unsigned dwStackSize, unsigned dwRetAsize);
+
+void determine_sequence(cmNode* _expr);
+
+template<class _Ret>
+class MyFunc_VA :public FuncBase {
+protected:
+	_Ret(*fptr) (char*);
+	char* _stack_ptr_before_call;
+public:
+	MyFunc_VA(_Ret(*Ptr)(char*), const char* _name) :FuncBase(_name) {
+		fptr = Ptr;
+		_stack_ptr_before_call = 0;
+		f_return_pointer = ::std::is_pointer<_Ret>::value;
+	}
+	virtual ~MyFunc_VA() {
+	}
+	virtual bool _is_va_func()const {
+		return true;
+	}
+	virtual void _call_func_ret_on_retarea()const override {
+		new(ret_area) _Ret(fptr(_stack_ptr_before_call));
+		_My_Stack_Poiner = _stack_ptr_before_call;
+	}
+	virtual void record_stack_pointer()override {
+		_stack_ptr_before_call = _My_Stack_Poiner;
+	}
+};
+
+#define MYFUNC(_FUNC_NAME_, _NAME_STR_) MyFunc<decltype(_FUNC_NAME_)>\
+(_FUNC_NAME_, _NAME_STR_)
+
+#define MYVA_FUNC(_FUNC_, _STR_NAME_) MyFunc_VA<\
+typename _Rt<decltype(_FUNC_)>::type>(_FUNC_,  _STR_NAME_)
+
+inline bool is_VA_end(char* _va_list) {
+	return _va_list == _My_Stack_Poiner;
+}
+
+inline void _VA_start(char*& _va_list, char* _a1) {
+	_va_list = _a1;
+}
+
+template<class T>
+inline T& _VA_arg(char*& _va_list) {
+	_va_list -= sizeof(T);
+	return *reinterpret_cast<T*>(_va_list);
+}
+
+MyVar CreateVector(char*  _A1);
+
+void procCommand_1(char* _Str_command);
+
+void implFuncInit();
+
+#endif // !_COMMAND_A_H_
